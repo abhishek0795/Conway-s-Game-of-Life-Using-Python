1dadad816111a77f8d49eaa90b8a0bcfd1ebf2b9
abhishek_95 <ajha@presidio.com>
2021-11-07 10:40:19 +0530

    modified content

--- cell_class.py
+++ cell_class.py
@@ -4,26 +4,26 @@ import pygame
 
 class Cell:
     def __init__(self, surface, grid_x, grid_y): #Intializing the variables.
-        self.alive = False               
-        self.surface = surface
-        self.grid_x = grid_x
-        self.grid_y = grid_y
+        self.alive = False      # Intializing the live neighbours boolean value to false.         
+        self.surface = surface  # Intializing the surface variable.
+        self.grid_x = grid_x    # Intializing the grid position of x-direction.
+        self.grid_y = grid_y    # Initializing the grid position of y-direction.
         self.image = pygame.Surface((20, 20))  # Size of the each grid cells.
-        self.reactangle = self.image.get_rect()
-        self.neighbours = []
-        self.alive_neighbours = 0
+        self.reactangle = self.image.get_rect() # Calling the rectangle image and storing in the rectangle variable.
+        self.neighbours = [] # Intializing the list of neighbours.
+        self.alive_neighbours = 0 # Intializing the live neighbours count.
 
-    def update(self):
-        self.reactangle.topleft = (self.grid_x * 20, self.grid_y * 20)
+    def update(self): # Update the grid cells.
+        self.reactangle.topleft = (self.grid_x * 20, self.grid_y * 20) # It will start updating the grid cells from the topleft corner of the game window.
     
     """ Draw function helps to draw the rectangle image and filling the colour if cell is alive. """
 
-    def draw(self):
+    def draw(self): # Draw the cell image
         try:
-            if self.alive:
-                self.image.fill((0,0,0)) 
+            if self.alive: 
+                self.image.fill((0,0,0)) # Filling the black colour image in reactangle shape if neigbours is alive.
             else:
-                self.image.fill((0, 0, 0))
+                # self.image.fill((0, 0, 0)) 
                 pygame.draw.rect(self.image, (255, 255, 255), (1, 1, 18, 18)) # Filling the cell colour and setting border width.
             self.surface.blit(self.image, (self.grid_x * 20, self.grid_y * 20)) # Draw the rectangle image on the grid cells.
         except(Exception):
@@ -31,12 +31,12 @@ class Cell:
 
     """ Get neighbours function helps to identify the neighbours of each grid cells. """
 
-    def get_neighbours(self, grid):
-        neighbour_list = [[1, 1], [-1, -1], [-1, 1], [1, -1], [0, -1], [0, 1],[1, 0], [-1, 0]]
+    def get_neighbours(self, grid): # Get the neighbours details
+        neighbour_list = [[1, 1], [-1, -1], [-1, 1], [1, -1], [0, -1], [0, 1],[1, 0], [-1, 0]] # Checking neighbours in all the eight directions.
         for neighbour in neighbour_list:
-            neighbour[0] += self.grid_x
-            neighbour[1] += self.grid_y
-        for neighbour in neighbour_list:
+            neighbour[0] += self.grid_x  # Updating the neighbours list of first position in grid x-direction.
+            neighbour[1] += self.grid_y  # Updating the neighbours list of first position in grid y-direction.
+        for neighbour in neighbour_list: 
             try:
                 if neighbour[0] < 0:
                     neighbour[0] += 30
@@ -56,7 +56,7 @@ class Cell:
 
     """ Live neighbours function checks whether the neighbours of grid cells are alive or not. """
 
-    def live_neighbours(self):
+    def live_neighbours(self): # Increamenting the count variable whenever live neighbour gets encountered.
         count = 0
         for neighbour in self.neighbours:
             try:

967fea4586d8a5ebd78dc2612a438240a546f10c
abhishek_95 <ajha@presidio.com>
2021-11-07 18:56:26 +0530

    modified content

--- button_class.py
+++ button_class.py
@@ -1,12 +1,14 @@
-import pygame
+""" Button class of the program """
+
+import pygame # importing the pygame module.
 
 vector = pygame.math.Vector2
 
 """ Creating button class with required settings when events happen after clicking on buttons. """
 
 class Button:
-    def __init__(self, surface, x, y, width, height, state='', id='', function=0, colour=(255, 255, 255), hover_colour=(255, 255, 255), border=True, border_width=2, border_colour=(0, 0, 0), text='', font_name='arial', text_size=20, text_colour=(0, 0, 0), bold_text=False):
-        self.type = 'button'
+    def __init__(self, surface, x, y, width, height, state='', id='', function=0, colour=(255, 255, 255), hover_colour=(255, 255, 255), border=True, border_width=2, border_colour=(0, 0, 0), text='', font_name='arial', text_size=20, text_colour=(0, 0, 0), bold_text=False): # Initializing the variables.
+        self.type = 'button' 
         self.x = x
         self.y = y
         self.position = vector(x, y)
@@ -32,9 +34,7 @@ class Button:
         self.hovered = False
         self.showing = True
 
-    """ Update function to update the game state when mouse hovering happens on different position. """
-
-    def update(self, position, game_state=''):
+    def update(self, position, game_state=''): # Updating the events based on the mouse cursor position and the game state.
         try:
             if self.mouse_hovering(position):
                 self.hovered = True
@@ -53,9 +53,7 @@ class Button:
         except(Exception):
             print("Exception: ", Exception)
 
-    """ Draw function to draw the rectangle on different grid cells. """
-    
-    def draw(self):
+    def draw(self): # Draw the image based on the changes occurs in the game window.
         try:
             if self.showing:
                 if self.border:
@@ -70,32 +68,26 @@ class Button:
 
                 if len(self.text) > 0:
                     self.show_text()
-                self.surface.blit(self.image, self.position) # Draw the rectangle image of the buttons icon
+                self.surface.blit(self.image, self.position) # Draw the rectangle image of the buttons icon.
         except(Exception):
             print("Exception: ", Exception)
 
-    """ Click function to check whether the click or hovering events happen on buttons. """
-
-    def click(self):
+    def click(self): # Click function helps to identify whether the mouse is hovered or not in the grid cells.
         try:
             if self.function != 0 and self.hovered:
                 self.function()
         except(Exception):
             print("Exception: ", Exception)
 
-    """ Show text function to set the font size and text colour of the text buttons. """
-
-    def show_text(self):
+    def show_text(self): # Show text function will set the text on the text button.
         font = pygame.font.SysFont(self.font_name, self.text_size, bold=self.bold_text)
         text = font.render(self.text, False, self.text_colour)
         size = text.get_size()
         x, y = self.width // 2 - (size[0] // 2), self.height // 2 - (size[1] // 2)
         position = vector(x, y)
-        self.image.blit(text, position)   # Draw the rectangle image on the grid cells
+        self.image.blit(text, position)   # Draw the rectangle image on the grid cells.
 
-    """ Mouse hovering function to check the hovering events based on the cursor position. """
-
-    def mouse_hovering(self, position):
+    def mouse_hovering(self, position): # Mouse hovering function to check the hovering events based on the cursor position.
         try:
             if self.showing:
                 if position[0] > self.position[0] and position[0] < self.position[0] + self.width:
